3283bf5b4936403af7226cdb507317ddd215f627
Jared Early <46874230+jaredwilliam@users.noreply.github.com>
2021-02-24 15:29:33 -0500

    Added static link tool material

---
+++ Static Link Tool/Components.py
@@ -0,0 +1,14 @@
+class Antenna():
+    
+    def __init__(self, name='', ant_type=''):
+        self.name = name
+        self.ant_type = ant_type
+        # Attributes
+        # Name, type, beamwidth, gain
+        
+
+    
+class Transmitter():
+    
+    def __init__(self, name = 'transmitter'):
+        self.name = name
